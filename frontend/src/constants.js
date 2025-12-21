3d4bb3a8a8d4edd8f3bddd230ed306cce1c7a496
Shamarvey1 <ajitlookforbest@gmail.com>
2025-12-21 19:00:20 +0530

    fixed api

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,3 @@
-let ENDPOINT;
-if(process.env.NODE_ENV === 'production'){
-  ENDPOINT = "https://booknest-1-hiti.onrender.com";
-} else {
-  ENDPOINT = "http://localhost:5001";
-}
+let ENDPOINT = process.env.VITE_API_URL;
 
 export { ENDPOINT };
